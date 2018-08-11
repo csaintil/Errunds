2f19f1b681859d816f882c590ed3e4792c23a08e
Jenniferlora <lorajennifer@aol.com>
2018-08-11 12:57:44 -0400

    blah

--- shop_leads.php
+++ shop_leads.php
@@ -1,30 +1,28 @@
 <?php
 // define variables and set to empty values
 
-// $fname = $lname = $email = $year = "";
+$fname = $lname = $email = $year = "";
 
-// if ($_SERVER["REQUEST_METHOD"] == "POST") {
-//   $fname = check_input($_POST["firstname"]);
-//   $lname = check_input($_POST["lastname"]);
-//   $mailfrom = check_input($_POST["email"]);
-//   $year = check_input($_POST["year"]);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  $fname = check_input($_POST["firstname"]);
+  $lname = check_input($_POST["lastname"]);
+  $mailfrom = check_input($_POST["email"]);
+  $year = check_input($_POST["year"]);
 
-// }
+}
 
-// function check_input($data) {
-//   $data = trim($data);
-//   $data = stripslashes($data);
-//   $data = htmlspecialchars($data);
-//   return $data;
+function check_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 
-// $mailTo = "jennifer@myerrunds.com";
+$mailTo = "jennifer@myerrunds.com";
 
-// $headers = "New Shopper Signed Up: " .$mailfrom;
-// $txt = "You have received an e-mail from ".$fname.".\n\n".;
+$headers = "New Shopper Signed Up: " .$mailfrom;
+$txt = "You have received an e-mail from ".$fname.".\n\n".;
 
-// mail($mailTo, $fname, $lname, $mailfrom,$year)
-// header("Location: index.html?mailsend")
-// }
-Hi <?php echo htmlspecialchars($_POST['name']); ?>.
-
+mail($mailTo, $fname, $lname, $mailfrom,$year)
+header("Location: index.html?mailsend")
+}
 ?>
